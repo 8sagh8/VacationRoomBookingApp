9ada12fad0a493337315bb3f6ab10de382206ea8
Sammar Abbas <sabbas21@myseneca.ca>
2019-11-04 00:00:35 -0500

    commit-set email and mongoose but not working

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ Name: Sammar Abbas
 ID: 157-597-170
 Email: sabbas21@myseneca.ca
 link: https://webapp322.herokuapp.com/
-*/// Importing 3rd Parties Modules / libraries
+*/
+// Importing 3rd Parties Modules / libraries
 const express = require (`express`);
 const exphbs = require (`express-handlebars`);
 const bodyParser = require(`body-parser`);
+const mongoose = require(`mongoose`);
 
 // Assigning variables
 const app = express();
@@ -14,12 +16,26 @@ const app = express();
 // load all static files inside folder public
 app.use(express.static(`public`));
 
+//to get values from forms
+app.use(bodyParser.urlencoded({extended: false}));
 
 // setting express-handlebars engine with express
 app.engine(`handlebars`, exphbs());
 app.set(`view engine`, `handlebars`);
 
-app.use(bodyParser.urlencoded({extended: false}));
+//This code is used to connect mongoose to our MONGODB in the Cloud
+const DBURL = 'mongodb+srv://userAdmin110:LostMongo110$@cluster0-swotc.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(DBURL, {useNewUrlParser: true})
+//Then block will only be executed if the above-mentioned line is successful
+.then(()=>{
+    console.log(`Database is connected`)
+})
+//The catch block will only be executed if the connection failed
+.catch(err=>{
+    console.log(`Something went wrong : ${err}`);
+})
+
 
 //Assigning Ports to templates Files
 //Index/Home Page
@@ -54,22 +70,6 @@ app.post(`/reg`, (req, res)=>{
     if(req.body.password == "" || req.body.password.length < 6 || req.body.password.length > 12)
         errors.push(`Please enter password between 6 to 12 characters only`);
 
-    if(req.body.password != "" && req.body.password.length > 5 && req.body.password.length < 13){
-        let alpha = "";
-        let num = -1;
-        for (let i = 0; i < req.body.password.length; ++i){
-            if (req.body.password[i] == [a-z][A-Z])
-                alpha = req.body.password[i];
-        
-            else if (req.body.password[i] == [0-9])
-                num = req.body.password[i];
-
-            else {}
-        }
-        if(alpha == "" || num < 0)
-            errors.push(`Password must have letters and numbers only!`);
-    }
-
     
     if(req.body.selectMonth == "Month" || req.body.selectDay == "Day" || req.body.selectYear == "Year")
         errors.push(`Incorrect Date input not allowed!`);
@@ -82,9 +82,79 @@ app.post(`/reg`, (req, res)=>{
     }
 
     else {
-        res.send(`Form is submitted Successfully!`);
+        const Schema = mongoose.Schema;
+
+        const taskSchema = new Schema({
+            email: String,
+            firstName: String,
+            lastName: String,
+            password: String,
+            selectMonth: String,
+            selectDay: Number,
+            selectYear: Number
+        });
+
+        //This creates a Model called Tasks. This model represents our Collection in our database
+        const Tasks = mongoose.model('Tasks', taskSchema);
+
+        const formData ={
+            email:req.body.email,
+            firstName:req.body.firstName,
+            lastName:req.body.lastName,
+            password:req.body.password,
+            selectMonth:req.body.selectMonth,
+            selectDay:req.body.selectDay,
+            selectYear:req.body.selectYear
+        }
+
+        //To create a  Task document we have to call the Model constructor
+        const ta = new Tasks(formData);
+        ta.save()
+        .then(() => {
+        console.log('Task was inserted into database')
+        })
+
+        .catch((err)=>{
+            console.log(`Task was not inserted into the database because ${err}`)
+        })
+
+
+        // Send Email Message
+        const nodemailer = require('nodemailer');
+        const sgTransport = require('nodemailer-sendgrid-transport');
         
+        const options = {
+            auth: {
+                api_key: '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'
+            }
+        }
+        
+        const mailer = nodemailer.createTransport(sgTransport(options));
+        
+        const email = {
+            to: `${req.body.email}`,
+            from: 'mastersagh@hotmail.com',
+            subject: 'SAMMAR ABBAS has registered you, Please login to book ROOMS',
+            text: `Dear ${req.body.firstName} ${req.body.lastName}, your email address: ${req.body.email} has been registered`,
+            html: `Dear ${req.body.firstName} ${req.body.lastName}, your email address: ${req.body.email} has been registered`
+        };
+                 
+        mailer.sendMail(email, (err, res)=> {
+            if (err) { 
+                console.log(err) 
+            }
+        
+           console.log(res);
+        });
+        
+        //REDIRECT THE USER TO THE DASHBOARD ROUTE
+        res.redirect("/dashboard");
     }
+})
+
+// Route for Dashboard
+app.get(`/dashboard`, (req, res)=>{
+    res.render("userDashboard");
 })
 
 //Login page
